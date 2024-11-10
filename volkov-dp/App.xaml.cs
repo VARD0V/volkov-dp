4190798fa3c1e888b35f32ca98c63edb9494b8b3
VARD0V <dimadima09876@gmail.com>
2024-11-10 20:13:48 +0700

    арпиварав

---
+++ volkov-dp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace volkov_dp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ volkov-dp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace volkov_dp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
